6e201c910b4e0f51edde6280fbc876bcf0de8ffb
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-04-27 12:18:06 +0700

    Update LCD.ino

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,12 +1,12 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define LCD_ADDRESS 0x3F  // Change to 0x3F if needed
+#define LCD_ADDRESS 0x3F  // Change to 0x27 if needed
 int trigPin = 10;
 int echoPin = 7;
-int s=A8;
+int s = A8;
 
-LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2); 
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -17,50 +17,26 @@ void setup() {
   lcd.setCursor(0, 0);
 
   Serial.begin(9600); // Initialize Serial Monitor
-  pinMode(s,INPUT);
+  pinMode(s, INPUT);
 }
 
 void loop() {
-  int D =analogRead(s);
- Serial.println(D);
-  delay(200);
- // float distance = getDistance(); // Measure distance
+  float distance = getDistance(); // Measure distance
+
+  // Display on Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 
   // Display on LCD
   lcd.clear();
   lcd.setCursor(0, 0);
+  lcd.print("Distance:");
+  lcd.setCursor(0, 1);
+  lcd.print(distance);
+  lcd.print(" cm");
 
-  lcd.print("light lavel: ");
-  lcd.print(D);
- 
-
-  if(D<230){
-  lcd.setCursor(0, 1);
-  lcd.print(" black");
-  }
-  else if (D>230 && D<=400){
-  lcd.setCursor(0, 1);
-  lcd.print(" blue");
-  }
-  else if (D>400 && D<=500){
-  lcd.setCursor(0, 1);
-  lcd.print("sky blue");
-  }
-  else if (D>500 && D<=600){
-  lcd.setCursor(0, 1);
-  lcd.print("green");
-  }
-  else{
-  lcd.setCursor(0, 1);
-  lcd.print("white");
-  }
-
-  // Display in Serial Monitor
- // Serial.print("Distance: ");
- // Serial.print(distance);
- // Serial.println(" cm");
-
-  //delay(500); 
+  delay(500);
 }
 
 float getDistance() {
