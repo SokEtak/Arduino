15d6d251adde22e5fab328eccf30269c339479db
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-04-28 19:57:23 +0700

    Update and rename TwoWheelsRobot.ino to TwoWheelsRobot_AutoDrive.ino
    Modified some part for better version

--- TwoWheelsRobot/TwoWheelsRobot_AutoDrive.ino
+++ TwoWheelsRobot/TwoWheelsRobot_AutoDrive.ino
@@ -1,11 +1,14 @@
+//Recommend with L293D driver motor control shield/AF Motor
+
 #include <AFMotor.h>
-// #include <NewPing.h>
-AF_DCMotor motor1(3);
-AF_DCMotor motor2(4);
-int echo = 9;
-int trig = 10;
-int irPin1 = A0;
-int irPin2 = A2;
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+
+int echoPin = 9;
+int trig_Pin = 10;
+int irPin_Right = A0;
+int irPin_Left = A2;
 
 double distance = 0;
 double duration = 0;
@@ -15,67 +18,66 @@ int irValue2 = 0;
 
 void setup() {
   Serial.begin(9600);
-  SpeedUp();
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  pinMode(irPin1, INPUT);
-  pinMode(irPin2, INPUT);
+  speedUp();
+  pinMode(trig_Pin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(irPin_Right, INPUT);
+  pinMode(irPin_Left, INPUT);
   delay(1000);
   Serial.println("Start!");
 
 }
-
+//Modifiable Function(Add More Logic)
 void loop() {
   distance = measureDistance();
   Serial.println(distance);
-  irValue1 = analogRead(irPin1);
-  irValue2 = analogRead(irPin2);
+  irValue1 = analogRead(irPin_Right);
+  irValue2 = analogRead(irPin_Left);
   Serial.print("irValue1: ");
   Serial.println(irValue1);
   Serial.print("irValue2: ");
   Serial.println(irValue2);
   delay(1000);
 }
-
+//function to get object distance
 double measureDistance() {
-  digitalWrite(trig, HIGH);
+  digitalWrite(trig_Pin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
+  digitalWrite(trig_Pin, LOW);
+  duration = pulseIn(echoPin, HIGH);
   distance = (duration / 2) / 28.5;
   return distance;
 }
 
-void Forward() {
+void forward() {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
 }
 
-void Backward() {
+void backward() {
   motor1.run(BACKWARD);
-  
   motor2.run(BACKWARD);
   
 }
-void Left() {
+void left() {
   motor2.run(BACKWARD);
   motor1.run(FORWARD);
   
 }
-void Right(){
+void right(){
   motor2.run(FORWARD);
   motor1.run(BACKWARD);
   
 }
-  void Stop() {
+  void stop() {
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   }
-  void SpeedUp(){
+  void speedUp(){
     motor1.setSpeed(255);
     motor2.setSpeed(255);
   }
-  void SpeedDown(){
+  void speedDown(){
     motor1.setSpeed(100);
     motor2.setSpeed(100);
   }
