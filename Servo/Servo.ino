2b5d643373793ce354039b38e4fdc9b9f14b8037
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-04-30 15:23:31 +0700

    Update Servo.ino

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,14 +1,16 @@
+//You Must Include This library To Make It Work Properly
 #include <Servo.h>
 
 const int servoPin = 6;
 Servo myServo;
 
 void setup() {
+  //Connect the servo motor to the Arduino, using digital pin 6 for the control signal
   myServo.attach(servoPin);
 }
 
 void loop() {
-  // Move from 0 to 180 degrees
+  // Move from 0 to 180 degrees(Forward)
   for (int angle = 0; angle <= 180; angle++) {
     myServo.write(angle);
     delay(15); // smooth movement
@@ -16,7 +18,7 @@ void loop() {
 
   delay(500); // pause at end
 
-  // Move from 180 to 0 degrees
+  // Move from 180 to 0 degrees(Backward)
   for (int angle = 180; angle >= 0; angle--) {
     myServo.write(angle);
     delay(15);
