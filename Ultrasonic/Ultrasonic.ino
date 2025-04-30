c9e03c70892ba8e6f101a1dce33c6c477321ae83
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-04-30 15:07:56 +0700

    Update Ultrasonic.ino
    Modified some line

--- Ultrasonic/Ultrasonic.ino
+++ Ultrasonic/Ultrasonic.ino
@@ -1,5 +1,8 @@
-const int trigPin = 9;
-const int echoPin = 10;
+int trigPin = 9;
+int echoPin = 10;
+ 
+long duration;
+float distanceCm;
 
 void setup() {
   Serial.begin(9600); 
@@ -8,9 +11,6 @@ void setup() {
 }
 
 void loop() {
-  long duration;
-  float distanceCm;
-
   // Clear trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -23,13 +23,13 @@ void loop() {
   // Read echoPin
   duration = pulseIn(echoPin, HIGH);
 
-  // Calculate distance in cm
+  // Calculate distance and converti to cm(centimeter)
   distanceCm = duration * 0.034 / 2;
 
   // Print distance
   Serial.print("Distance: ");
   Serial.print(distanceCm);
   Serial.println(" cm");
-
+  //delay 500ms(1/2 second)
   delay(500);
 }
