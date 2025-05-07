46410ee7721bd6d556eca3f5182fc1714133c887
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-05-07 15:16:04 +0700

    Update TwoWheelsRobot_AutoDrive.ino

--- TwoWheelsRobot/TwoWheelsRobot_AutoDrive.ino
+++ TwoWheelsRobot/TwoWheelsRobot_AutoDrive.ino
@@ -1,6 +1,6 @@
 //Recommend with L293D driver motor control shield/AF Motor
 #include <AFMotor.h>
-
+//Motor Setup
 AF_DCMotor motor1(1);
 AF_DCMotor motor3(3);
 
@@ -14,7 +14,6 @@ double duration = 0;
 int ir_RightValue = 0;           
 int ir_LeftValue = 0;
 
-
 void setup() {
   Serial.begin(9600);
   speedUp();
@@ -24,20 +23,57 @@ void setup() {
   pinMode(ir_LeftPin, INPUT);
   delay(1000);
   Serial.println("Start!");
-
 }
-//Modifiable Function(Add More Logic)
+
+//Modifiable Function(Add Some Logic)
 void loop() {
-  distance = measureDistance();
-  Serial.println(distance);
-  ir_RightValue = analogRead(ir_RightPin);
-  ir_LeftValue = analogRead(ir_LeftPin);
+  //outout betwen 1(detect white color) and 0 (detect black color)
+  ir_RightValue = digitalRead(ir_RightPin);
+  ir_LeftValue = digitalRead(ir_LeftPin);
   Serial.print("ir_RightValue: ");
   Serial.println(ir_RightValue);
   Serial.print("ir_LeftValue: ");
   Serial.println(ir_LeftValue);
   delay(1000);//wait 1 second
   //write your logic here(e.g,if ir_right detected white color:do something,...)
+  
+  //if both ir detected at the same time
+  if(ir_RightValue == 1 && ir_LeftValue == 1){
+     //move backward
+
+      //wait
+
+  }else if(ir_RightValue == 1){
+    //move backward
+
+    //wait
+
+    //turn left 
+
+    //wait
+
+  }else if(ir_LeftValue == 1){
+    //wait
+
+    //turn right 
+
+    //wait
+
+
+  }else{
+    //move forward
+  }
+  
+  //get distance from ultrasonic
+  double distance = measureDistance();
+
+  //ulltra
+  if(distance<=50){
+    //do something
+  }else if(){
+    //do something
+  }
+  // add another logic here
   
 }
 //function to get object distance
@@ -46,11 +82,13 @@ double measureDistance() {
   delayMicroseconds(10);
   digitalWrite(trig_Pin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration / 2) / 28.5;
+  //distance in centimeters
+  distance = (duration / 2) * 0.0343;
+
   return distance;
 }
 
-void forward() {
+void moveForward() {
   motor1.run(FORWARD);
   motor3.run(FORWARD);
 }
@@ -74,6 +112,7 @@ void right(){
   motor1.run(RELEASE);
   motor3.run(RELEASE);
   }
+
   void speedUp(){
     motor1.setSpeed(255);
     motor3.setSpeed(255);
@@ -81,4 +120,8 @@ void right(){
   void speedDown(){
     motor1.setSpeed(100);
     motor3.setSpeed(100);
+  }
+
+  void wait(int millisecond){
+    delay(millisecond);
   }
