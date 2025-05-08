a70908a20fa1af8ab543de6ba94f53c050f179b1
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-05-08 14:38:48 +0700

    Update TwoWheelsRobot_Senior.ino

--- TwoWheelsRobot/TwoWheelsRobot_Senior.ino
+++ TwoWheelsRobot/TwoWheelsRobot_Senior.ino
@@ -1,8 +1,8 @@
 //Recommend with L293D driver motor control shield/AF Motor
 #include <AFMotor.h>
 //Motor Setup
-AF_DCMotor motor1(1);
-AF_DCMotor motor3(3);
+AF_DCMotor m1(1);
+AF_DCMotor m3(3);
 
 int echoPin = 9;
 int trig_Pin = 10;
@@ -34,7 +34,7 @@ void loop() {
   Serial.println(ir_RightValue);
   Serial.print("ir_LeftValue: ");
   Serial.println(ir_LeftValue);
-  delay(1000);//wait 1 second
+  
   //write your logic here(e.g,if ir_right detected white color:do something,...)
   
   //if both ir detected at the same time
@@ -64,7 +64,7 @@ void loop() {
     //move forward
   }
   
-  //get distance from ultrasonic
+  //get distance from ultrasonic via measureDistance funcion
   double distance = measureDistance();
 
   //ulltra
@@ -89,37 +89,37 @@ double measureDistance() {
 }
 
 void moveForward() {
-  motor1.run(FORWARD);
-  motor3.run(FORWARD);
+  m1.run(FORWARD);
+  m3.run(FORWARD);
 }
 
 void backward() {
-  motor1.run(BACKWARD);
-  motor3.run(BACKWARD);
+  m1.run(BACKWARD);
+  m3.run(BACKWARD);
   
 }
-void left() {
-  motor3.run(BACKWARD);
-  motor1.run(FORWARD);
+void turnLeft() {
+  m3.run(BACKWARD);
+  m1.run(FORWARD);
   
 }
-void right(){
-  motor3.run(FORWARD);
-  motor1.run(BACKWARD);
+void turnRight(){
+  m3.run(FORWARD);
+  m1.run(BACKWARD);
   
 }
   void stop() {
-  motor1.run(RELEASE);
-  motor3.run(RELEASE);
+  m1.run(RELEASE);
+  m3.run(RELEASE);
   }
 
   void speedUp(){
-    motor1.setSpeed(255);
-    motor3.setSpeed(255);
+    m1.setSpeed(255);
+    m3.setSpeed(255);
   }
   void speedDown(){
-    motor1.setSpeed(100);
-    motor3.setSpeed(100);
+    m1.setSpeed(100);
+    m3.setSpeed(100);
   }
 
   void wait(int millisecond){
